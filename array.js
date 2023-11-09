8ec72950c1f8523a7984a84b35cf9d65f7a312d7
MarouaneBRomdhane <133340397+MarouaneBRomdhane@users.noreply.github.com>
2023-11-09 12:33:50 +0100

    nothing done honestly, just adding more comments

--- array.js
+++ array.js
@@ -1,23 +1,23 @@
-const names = ['John', 'Jane', 'Sam', 'Melissa'];
+const names = ["John", "Jane", "Sam", "Melissa"];
 
 // for loop
 for (let i = 0; i < names.length; i++) {
   const element = names[i];
   console.log(element);
 }
-console.log('------------------');
+console.log("------------------");
 // for-of
 for (const element of names) {
   console.log(element);
 }
-console.log('------------------');
+console.log("------------------");
 // Array.prototype.forEach
-names.forEach(element => console.log(element));
+names.forEach((element) => console.log(element));
 // console.log(res);
-console.log('------------------');
+console.log("------------------");
 // Array.prototype.map
 
-const newArr = names.map(element => {
+const newArr = names.map((element) => {
   console.log(`Hello ${element}`);
   return `Hello ${element}`;
 });
@@ -25,7 +25,7 @@ console.log(newArr);
 
 // Array.prototype.filter
 const numbers = [1, 2, 3, 4];
-const pairs = numbers.filter(element => element % 2 === 0);
+const pairs = numbers.filter((element) => element % 2 === 0);
 console.log(pairs);
 
 // Array.prototype.reduce
@@ -37,8 +37,12 @@ console.log(x);
 
 // Array.prototype.find
 
-const found = names.find(element => element.length === 4);
+const found = names.find((element) => element.length === 4);
 console.log(found);
 
-const foundIndex = names.findIndex(element => element.length === 4);
+const foundIndex = names.findIndex((element) => element.length === 4);
 console.log(foundIndex);
+
+// there is also array.join() that works with strings and adds a space in the concatenation. check MDN for more info
+
+//
